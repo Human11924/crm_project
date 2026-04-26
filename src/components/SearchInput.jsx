a9f409c29f3be829c25e02acb7ee78465bc32d2a
un12043 <un12043@auca.kg>
2026-04-26 15:45:41 +0600

    Create SearchInput.jsx

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react"
+import { useDebounce } from "../hooks/useDebounce"
+
+export default function SearchInput({ onSearch }) {
+  const [value, setValue] = useState("")
+  const debouncedValue = useDebounce(value, 400)
+
+  useEffect(() => {
+    onSearch(debouncedValue)
+  }, [debouncedValue])
+
+  return (
+    <input
+      type="text"
+      placeholder="Поиск..."
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  )
+}
